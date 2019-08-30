6b9714cafe4d4856e206a9dba6db339cd238fc8a
ljie@knights.ucf.edu <ljie@knights.ucf.edu>
2019-08-29 22:38:22 -0400

    Merge remote-tracking branch 'origin/master'
    Signed-off-by: ljie@knights.ucf.edu <ljie@knights.ucf.edu>

--- benchmark/mnist.py
+++ benchmark/mnist.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 
 import argparse
+import shutil
 
 import torch
 import torch.nn as nn
@@ -12,11 +13,39 @@ from python.torx.module.layer import crxb_Conv2d
 from python.torx.module.layer import crxb_Linear
 
 
+def save_checkpoint(state, is_best, filename):
+    torch.save(state, filename)
+    if is_best:
+        shutil.copyfile(filename, 'model_best.pth.tar')
+
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
 class Net(nn.Module):
     def __init__(self, crxb_size, gmin, gmax, vdd, ir_drop, device, scaler_dw):
         super(Net, self).__init__()
-        #   self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        #   self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        # self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        # self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        # self.conv2_drop = nn.Dropout2d()
+        # self.fc1 = nn.Linear(320, 50)
+        # self.fc2 = nn.Linear(50, 10)
         self.conv1 = crxb_Conv2d(1, 10, kernel_size=5, crxb_size=crxb_size, scaler_dw=scaler_dw,
                                  gmax=gmax, gmin=gmin, vdd=vdd, ir_drop=ir_drop, device=device)
         self.conv2 = crxb_Conv2d(10, 20, kernel_size=5, crxb_size=crxb_size,
@@ -26,8 +55,7 @@ class Net(nn.Module):
                                gmax=gmax, gmin=gmin, vdd=vdd, ir_drop=ir_drop, device=device)
         self.fc2 = crxb_Linear(50, 10, crxb_size=crxb_size, scaler_dw=scaler_dw,
                                gmax=gmax, gmin=gmin, vdd=vdd, ir_drop=ir_drop, device=device)
-        #   self.fc1 = nn.Linear(320, 50)
-        #   self.fc2 = nn.Linear(50, 10)
+
 
     def forward(self, x):
         x = F.relu(F.max_pool2d(self.conv1(x), 2))
@@ -40,50 +68,120 @@ class Net(nn.Module):
         return F.log_softmax(x, dim=1)
 
 
-def train(args, model, device, train_loader, optimizer, epoch):
+# def train(args, model, device, train_loader, optimizer, criterion, epoch):
+#     losses = AverageMeter()
+#
+#     model.train()
+#     correct = 0
+#
+#     for batch_idx, (data, target) in enumerate(train_loader):
+#
+#         for name, module in model.named_modules():
+#             if isinstance(module, crxb_Conv2d) or isinstance(module, crxb_Linear):
+#                 module._reset_delta()
+#
+#         data, target = data.to(device), target.to(device)
+#         optimizer.zero_grad()
+#         output = model(data)
+#         loss = criterion(output, target)
+#         losses.update(loss.item(), data.size(0))
+#         loss.backward()
+#         optimizer.step()
+#
+#         pred = output.max(1, keepdim=True)[1]  # get the index of the max log-probability
+#         correct += pred.eq(target.view_as(pred)).sum().item()
+#
+#         if batch_idx % args.log_interval == 0:
+#             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+#                 epoch, batch_idx * len(data), len(train_loader.dataset),
+#                        100. * batch_idx / len(train_loader), loss.item()))
+#
+#     return losses.avg
+
+def train(model, device, criterion, optimizer, train_loader, epoch):
+    losses = AverageMeter()
+
     model.train()
+    correct = 0
+
     for batch_idx, (data, target) in enumerate(train_loader):
+
+        for name, module in model.named_modules():
+            if isinstance(module, crxb_Conv2d) or isinstance(module, crxb_Linear):
+                module._reset_delta()
+
         data, target = data.to(device), target.to(device)
         optimizer.zero_grad()
         output = model(data)
-        loss = F.nll_loss(output, target)
+        loss = criterion(output, target)
+        losses.update(loss.item(), data.size(0))
         loss.backward()
         optimizer.step()
-        if batch_idx % args.log_interval == 0:
+        pred = output.max(1, keepdim=True)[1]  # get the index of the max log-probability
+        correct += pred.eq(target.view_as(pred)).sum().item()
+
+        if batch_idx % 10 == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
+                epoch, batch_idx * len(data), train_loader.sampler.__len__(),
                        100. * batch_idx / len(train_loader), loss.item()))
 
+    print('\nTrain set: Accuracy: {}/{} ({:.4f}%)\n'.format(
+        correct, train_loader.sampler.__len__(),
+        100. * correct / train_loader.sampler.__len__()))
 
-def test(args, model, device, test_loader):
+    return losses.avg
+
+
+# def test(args, model, device, test_loader):
+#     model.eval()
+#     test_loss = 0
+#     correct = 0
+#     with torch.no_grad():
+#         for data, target in test_loader:
+#             data, target = data.to(device), target.to(device)
+#             output = model(data)
+#             test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
+#             pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+#             correct += pred.eq(target.view_as(pred)).sum().item()
+#
+#     test_loss /= len(test_loader.dataset)
+#
+#     print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+#         test_loss, correct, len(test_loader.dataset),
+#         100. * correct / len(test_loader.dataset)))
+
+
+def validate(model, device, criterion, val_loader):
     model.eval()
     test_loss = 0
     correct = 0
     with torch.no_grad():
-        for data, target in test_loader:
+
+        for batch_idx, (data, target) in enumerate(val_loader):
             data, target = data.to(device), target.to(device)
             output = model(data)
-            test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
-            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            test_loss += criterion(output, target).item()
+            pred = output.max(1, keepdim=True)[1]  # get the index of the max log-probability
             correct += pred.eq(target.view_as(pred)).sum().item()
 
-    test_loss /= len(test_loader.dataset)
+        test_loss /= len(val_loader.dataset)
+        print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.4f}%)\n'.format(
+            test_loss, correct, val_loader.sampler.__len__(),
+            100. * correct / val_loader.sampler.__len__()))
 
-    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-        test_loss, correct, len(test_loader.dataset),
-        100. * correct / len(test_loader.dataset)))
+        return test_loss
 
 
 def main():
     # Training settings
     parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
-    parser.add_argument('--batch-size', type=int, default=64, metavar='N',
+    parser.add_argument('--batch-size', type=int, default=1000, metavar='N',
                         help='input batch size for training (default: 64)')
     parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
                         help='input batch size for testing (default: 1000)')
     parser.add_argument('--epochs', type=int, default=1, metavar='N',
                         help='number of epochs to train (default: 10)')
-    parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
+    parser.add_argument('--lr', type=float, default=0.05, metavar='LR',
                         help='learning rate (default: 0.01)')
     parser.add_argument('--momentum', type=float, default=0.5, metavar='M',
                         help='SGD momentum (default: 0.5)')
@@ -113,13 +211,8 @@ def main():
 
     args = parser.parse_args()
 
-    # parameters
-    ir_drop = args.ir_drop
-    crxb_size = args.crxb_size
-    gmax = args.gmax
-    gmin = args.gmin
-    vdd = args.vdd
-    scaler_dw = args.scaler_dw
+    best_error = 0
+
 
     use_cuda = not args.no_cuda and torch.cuda.is_available()
 
@@ -135,6 +228,7 @@ def main():
                            transforms.Normalize((0.1307,), (0.3081,))
                        ])),
         batch_size=args.batch_size, shuffle=True, **kwargs)
+
     test_loader = torch.utils.data.DataLoader(
         datasets.MNIST('../data', train=False, transform=transforms.Compose([
             transforms.ToTensor(),
@@ -142,16 +236,46 @@ def main():
         ])),
         batch_size=args.test_batch_size, shuffle=True, **kwargs)
 
-    model = Net(crxb_size=crxb_size, gmax=gmax, gmin=gmin,
-                vdd=vdd, ir_drop=ir_drop, device=device, scaler_dw=scaler_dw).to(device)
+    model = Net(crxb_size=args.crxb_size, gmax=args.gmax, gmin=args.gmin,
+                vdd=args.vdd, ir_drop=args.ir_drop, device=device, scaler_dw=args.scaler_dw).to(device)
     optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
-
+    criterion = torch.nn.CrossEntropyLoss().to(device)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.1,
+                                                           patience=10, verbose=True, threshold=0.05,
+                                                           threshold_mode='rel', min_lr=1e-4)
+    loss_log = []
     for epoch in range(1, args.epochs + 1):
-        train(args, model, device, train_loader, optimizer, epoch)
-        test(args, model, device, test_loader)
+        print(epoch, optimizer.param_groups[0]['lr'])
 
-    if (args.save_model):
-        torch.save(model.state_dict(), "mnist_cnn.pt")
+        train_loss = train(model=model, device=device, criterion=criterion,
+                           optimizer=optimizer, train_loader=train_loader,
+                           epoch=epoch)
+        val_loss = validate(model=model, device=device, criterion=criterion,
+                            val_loader=test_loader)
+
+        lr_last = optimizer.param_groups[0]['lr']
+        scheduler.step(val_loss)
+        lr_now = optimizer.param_groups[0]['lr']
+
+        # break the training
+        if optimizer.param_groups[0]['lr'] < ((scheduler.min_lrs[0] / scheduler.factor) + scheduler.min_lrs[0]) / 2:
+            print("Accuracy not improve anymore, stop training!")
+            break
+
+        loss_log += [(epoch, train_loss, val_loss)]
+        is_best = val_loss > best_error
+        best_error = min(val_loss, best_error)
+
+        filename = 'checkpoint_' + str(args.crxb_size) + '.pth.tar'
+        save_checkpoint(state={
+            'epoch': epoch + 1,
+            'state_dict': model.state_dict(),
+            'best_acc1': best_error,
+            'optimizer': optimizer.state_dict(),
+        }, is_best=is_best, filename=filename)
+
+    # if (args.save_model):
+    #     torch.save(model.state_dict(), "mnist_cnn.pt")
 
 
 if __name__ == '__main__':
